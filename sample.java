b1f500c87c4a9e425346f396d757cbd082061820
Haricharan <151445781+haricharan123461@users.noreply.github.com>
2025-12-02 10:58:51 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,18 @@
+public class sample {
+
+    public static void main(String[] args) {
+
+        int marks[] = {85, 90, 75, 88, 92};  
+
+        int total = 0;
+
+        for (int i = 0; i < marks.length; i++) {
+            total += marks[i];
+        }
+
+        double average = (double) total / marks.length;
+
+        System.out.println("Total Marks = " + total);
+        System.out.println("Average Marks = " + average);
+    }
+}
